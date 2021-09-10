9f422cb61e7e9ab0c1e51a1db093a35fc25872a3
pschake34 <pschake34@charlottesvilleschools.org>
2021-09-10 15:54:59 -0400

    Fix the fading

--- ColorFade/ColorFade.py
+++ ColorFade/ColorFade.py
@@ -1,5 +1,6 @@
 import board
 import neopixel
+import time
 
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.1)
 
@@ -7,34 +8,36 @@ r = 0
 g = 0
 b = 0
 
-interval = 3
+interval = 1
 trigger = False
 
 while True:
-    if r <= 100:
-        r += interval
+    if not trigger:
+        if r <= 100:
+            r += interval
 
-    elif r >= 100 and g <= 100:
-        g += interval
+        elif r >= 100 and g <= 100:
+            g += interval
 
-    elif r and g >= 100 and b <= 100:
-        b += interval
+        elif r and g >= 100 and b <= 100:
+            b += interval
 
-    elif r and g and b >= 100 and trigger == False:
-        trigger = True
+        elif r and g and b >= 100:
+            trigger = True
 
     if trigger:
-        if g and b >= 100:
+        if g and b >= 100 and r > 0:
             r -= interval
 
-        elif b >= 100:
+        elif b >= 100 and r == 0 and g > 0:
             g -= interval
 
-        elif r and g < 100:
-            b -= 100
+        elif r == 0 and g == 0 and b > 0:
+            b -= interval
 
         else:
             trigger = False
 
     dot.fill((r, g, b))
     print(r, b, g)
+    time.sleep(0.02)
