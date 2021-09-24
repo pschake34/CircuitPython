92302527150eb45c75e8dd269ac4751204f7df1f
pschake34 <pschake34@charlottesvilleschools.org>
2021-09-24 15:38:22 -0400

    FInal version of DistanceSensor

--- DistanceSensor/DistanceSensor.py
+++ DistanceSensor/DistanceSensor.py
@@ -1,7 +1,6 @@
 import time
 import board
 import adafruit_hcsr04
-import math
 import neopixel
 
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
@@ -13,36 +12,37 @@ b = 0
 
 distance = 0
 
-def fade(range0, range1, value):
-    distance = range1 - range0
-    color0 = abs(((value - range0) / distance) * 255)
-    color1 = distance - value
-    if color1 < 0:
-        color1 = 0
-    return color0, color1
+def fade(lower, upper, val):    # gives the values needed to fade two LEDs between a user set range
+    real_upper = upper - lower
+    result1 = (abs(val - lower) / real_upper) * 255
+    result2 = 255 - result1
+    print("Result 1: " + str(result1))
+    print("Result 2: " + str(result2))
+    print("Value: " + str(val))
+    return result1, result2
+    
 
 while True:
     try:
-        #print(sonar.distance)
         distance = sonar.distance
 
-        if distance <= 5:
+        if distance <= 5:   # makes LED red when distance <= 5
             r = 255
             g = 0
             b = 0
-        elif distance <= 20:
+        elif distance <= 20:    # makes LED fade from red to blue while distance is between 5 and 20
             values = fade(5, 20, distance)
             b = int(values[0])
             r = int(values[1])
             g = 0
-        elif distance <= 35:
+        elif distance <= 35:    # makes LED fade from blue to green when distance between 20 and 35
             values = fade(20, 35, distance)
             g = int(values[0])
             b = int(values[1])
             r = 0
-        else:
-            r = 255
-            g = 0
+        else:       # makes LED green when distance > 35
+            r = 0
+            g = 255
             b = 0
     except RuntimeError:
         print("Retrying...")
