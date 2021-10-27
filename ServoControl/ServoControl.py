06405f58a7a1991b0f957555047f5f7f2c005c31
pschake34 <pschake34@charlottesvilleschools.org>
2021-10-26 21:38:07 -0400

    Add comments to ServoControl

--- ServoControl/ServoControl.py
+++ ServoControl/ServoControl.py
@@ -1,3 +1,6 @@
+# Paul Schakel
+# Moves a servo back and forth using two wires with capacative touch
+
 import time
 import board
 import pwmio
@@ -15,25 +18,23 @@ pulse_ms = 1.0
 increment = 0
 delay = 0.005
 
-# Create a function to simplify setting PWM duty cycle for the servo:
-def servo_duty_cycle(pulse_ms, frequency=50):
+def servo_duty_cycle(pulse_ms, frequency=50): # simplifies setting PWM duty cycle for the servo
     period_ms = 1.0 / frequency * 1000.0
     duty_cycle = int(pulse_ms / (period_ms / 65535.0))
     return duty_cycle
 
 while True:
-    servo.duty_cycle = servo_duty_cycle(pulse_ms)
+    servo.duty_cycle = servo_duty_cycle(pulse_ms)   # set the position of the servo
     time.sleep(delay)
 
-    increment = 0
-
-    if touch_left.value:
+    if touch_left.value: # if left wire has been touched, turn left
         print("Moving Left")
         increment = 0.005
-    elif touch_right.value:
+    elif touch_right.value: # if right wire has been touched, turn right
         print("Moving Right")
         increment = -0.005
 
+    # if the values for the directional limits go past the capability of the servo, set them to a value before those limits
     if pulse_ms > 2.4:
         pulse_ms = 2.4
     elif pulse_ms < 0.6:
