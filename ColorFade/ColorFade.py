9809147d88f623904102db5b5afa9163f1756d8c
pschake34 <pschake34@charlottesvilleschools.org>
2021-10-26 21:38:16 -0400

    Add comments to ColorFade

--- ColorFade/ColorFade.py
+++ ColorFade/ColorFade.py
@@ -1,3 +1,6 @@
+# Paul Schakel
+# Fades the colors on a neopixel in and out in a set order
+
 import board
 import neopixel
 import time
@@ -12,30 +15,30 @@ interval = 1
 trigger = False
 
 while True:
-    if not trigger:
-        if r <= 100:
+    if not trigger: # if the colors should be fading in
+        if r <= 100: # fade in red
             r += interval
 
-        elif r >= 100 and g <= 100:
+        elif r >= 100 and g <= 100: # if red is faded in, fade in green
             g += interval
 
-        elif r and g >= 100 and b <= 100:
+        elif r and g >= 100 and b <= 100: # if green and red have been faded in, fade in blue
             b += interval
 
-        elif r and g and b >= 100:
+        elif r and g and b >= 100: # if all the colors are faded in, fade them out
             trigger = True
 
-    if trigger:
-        if g and b >= 100 and r > 0:
+    if trigger: # if the colors should be fading out
+        if g and b >= 100 and r > 0: # if green, blue and red are faded in, fade our red
             r -= interval
 
-        elif b >= 100 and r == 0 and g > 0:
+        elif b >= 100 and r == 0 and g > 0: # if green and blue are faded in and red isn't, fade our green 
             g -= interval
 
-        elif r == 0 and g == 0 and b > 0:
+        elif r == 0 and g == 0 and b > 0: # if green and red have been faded out, fade our blue
             b -= interval
 
-        else:
+        else: # if all the colors are faded our, fade them in again
             trigger = False
 
     dot.fill((r, g, b))
